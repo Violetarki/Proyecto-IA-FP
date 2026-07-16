5bafb72e5fc8c7e6fa58ec947969088821f515d6
aurasm90 <111882449+aurasm90@users.noreply.github.com>
2026-07-16 16:06:31 +0200

    Update document_loader.py

--- src/document_loader.py
+++ src/document_loader.py
@@ -12,19 +12,31 @@ def leer_documentos(carpeta: str) -> list[Documento]:
     """
 
     # Recibir la carpeta
+    ruta = Path(carpeta)
 
     # Comprobar que la carpeta existe
+    if not ruta.is_dir():
+        raise NotADirectoryError(f"La ruta no es una carpeta: {ruta}")
+
+    if not ruta.exists():
+        raise FileNotFoundError(f"No existe la carpeta: {ruta}")
 
     # Buscar todos los PDFs
-    # Posibilidad de usar ruta.glob("*.pdf") --> "Búscame todos los archivos que cumplan un patrón."
+    # --> "Búscame todos los archivos que cumplan un patrón."
 
     # Crear una lista vacía
     documentos = []
 
     # Para cada PDF:
-
-    # 1. llamar a leer_pdf()
-
-    # 2. guardar el Documento en la lista
+    for archivo in ruta.glob("*.pdf"):
+        documento = leer_pdf(archivo)
+        documentos.append(documento)
+   
 
     # Devolver la lista
+    return documentos
+
+
+if __name__ == "__main__":
+    ruta_docs = "documents"
+    leer_documentos(ruta_docs)
