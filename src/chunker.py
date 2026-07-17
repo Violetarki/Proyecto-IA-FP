c3c51f02f1143d960ac2a779421382cffe4f733f
aurasm90 <111882449+aurasm90@users.noreply.github.com>
2026-07-17 11:41:17 +0200

    Update chunker.py

--- src/chunker.py
+++ src/chunker.py
@@ -1,2 +1,49 @@
 """Se encarga de dividir un Documento en una lista de Chunk, 
 procurando que cada uno represente una unidad coherente de conocimiento."""
+
+
+# Entrada:
+# Documento
+
+# Salida:
+# list[Chunk]
+
+# Ej.:
+# Documento
+#       │
+#       ▼
+# chunker
+#       │
+#       ▼
+# Chunk
+# Chunk
+# Chunk
+# Chunk
+
+# cómo detecta un título:
+# Opción A — Detectar patrones conocidos: Síntesis, etc
+# Opción B — Detectar por formato: línea corta; no acaba en punto (aunque hay excepciones); está rodeada de líneas vacías.
+# Opción C — Mezcla de ambas
+
+
+# Podemos tener una lista de patrones como:
+
+TITULOS = {
+    "Conceptos básicos",
+    "Simulación empresarial",
+    "Síntesis",
+    "Test de repaso",
+    "Comprueba tu aprendizaje",
+    "Resultados de aprendizaje",
+    "Contenidos básicos",
+}
+# qué hace cuando encuentra uno,
+
+# cómo decide que un chunk es demasiado grande.
+
+# Propuesta de arquitectura
+# chunker.py
+
+# ├── crear_chunks() --> Coordina el algoritmo.
+# ├── es_titulo() --> T/F ¿Esta línea es un título?
+# ├── obtener_seccion() --> Cuando detectamos un título, devuelve el texto que guardaremos en: chunk.seccion
