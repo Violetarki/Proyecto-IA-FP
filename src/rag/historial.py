25d556f4b9e0b1445a532ae8d33952a012734144
Violetarki <violetadmo@gmail.com>
2026-07-29 11:40:07 +0200

    crear clase Historial

---
+++ src/rag/historial.py
@@ -0,0 +1,15 @@
+
+
+class Historial:
+
+    def obtener_historial(self, id_conversacion):
+        ...
+
+    def añadir_mensaje_usuario():
+        ...
+
+    def añadir_respuesta():
+        ...
+
+    def limpiar_historial():
+        ...
