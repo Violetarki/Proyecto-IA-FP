8cc5326aaf8724171f116c0e2482aa5fbb6e8555
aurasm90 <111882449+aurasm90@users.noreply.github.com>
2026-07-31 11:46:32 +0200

    Update test_historial.py

--- tests/rag/test_historial.py
+++ tests/rag/test_historial.py
@@ -31,6 +31,21 @@ class TestHistorial(unittest.TestCase):
         # Después:
         # obtener_historial("123")
         # Debe devolver un mensaje con esos datos.
+        
+    def test_agregar_varios_mensajes(self):
+        pass
+    
+    
+    def test_obtener_contexto(self):
+        pass
+    
+    def test_eliminar_conversacion(self):
+        pass
+    
+    def test_conversaciones_independientes(self):
+        pass
+    
+    
 
     def tearDown(self):
         """Se ejecuta al terminar cada test."""
