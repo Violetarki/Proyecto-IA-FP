ca1775dcf8772eba67516654b4f539c6a3ddbc2f
Violetarki <violetadmo@gmail.com>
2026-08-10 15:04:11 +0200

    los classifier y esqueleto de guided_mode

---
+++ src/rag/guided_mode.py
@@ -0,0 +1,14 @@
+
+
+class ModoGuiado:
+    def iniciar(self, nodo_proceso):
+        ...
+
+    def siguiente_paso(self):
+        ...
+
+    def esta_activo(self):
+        ...
+
+    def procesar_respuesta(self, respuesta_usuario):
+        ...
