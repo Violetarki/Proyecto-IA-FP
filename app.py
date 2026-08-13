22de518537882e8127406bd2ab4197f01ead4916
Violetarki <violetadmo@gmail.com>
2026-08-13 15:01:52 +0200

    lo del puerto

--- app.py
+++ app.py
@@ -53,6 +53,5 @@ app.secret_key = os.getenv(
 
 
 if __name__ == "__main__":
-    app.run(
-        debug=True
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
