1c89548ae7d216acd9b175fadbf7d223c0de3923
Violetarki <violetadmo@gmail.com>
2026-08-13 15:09:14 +0200

    print para probar

--- app.py
+++ app.py
@@ -54,4 +54,5 @@ app.secret_key = os.getenv(
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    print(f"ARRANCANDO FLASK EN EL PUERTO {port}")
     app.run(host="0.0.0.0", port=port)
